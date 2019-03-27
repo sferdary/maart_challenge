fb01d662d5a7dcfa39b5aac5c29b41ca77060fdc
S. Ferdary <sferdary@n8xxel>
2019-03-27 12:47:31 +0100

    dashboard layout update

--- resources/views/pages/Admin/dashboard.php
+++ resources/views/pages/Admin/dashboard.php
@@ -5,7 +5,7 @@ if (isset($_SESSION['logged_in'])) {
 <div class="row justify-content-center">
     <div class="col-md-3 mt-3">
         <div class="card">
-            <div class="card-header bg-dark text-white">Create real estate</div>
+            <div class="card-header">Create real estate</div>
             <div class="card-body text-center">
                 <a href="?url=create-real-estate"><img src="assets/img/layouts/house.png" style="width:100%; height: auto;" alt=""></a>
             </div>
@@ -14,7 +14,7 @@ if (isset($_SESSION['logged_in'])) {
 
     <div class="col-md-3 mt-3">
         <div class="card">
-            <div class="card-header bg-dark text-white">Create new account</div>
+            <div class="card-header">Create new account</div>
             <div class="card-body text-center">
             <a href="?url=register"><img src="assets/img/layouts/human_resources.png" style="width:100%; height: auto;" alt=""></a>
             </div>
